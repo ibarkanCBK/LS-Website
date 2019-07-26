40ddd68a965d02089d1ba5e64f5ef34b5c7b191d
ibarkanCBK <52602129+ibarkanCBK@users.noreply.github.com>
2019-07-26 16:09:55 -0400

    First Commit- Lion's Story Redesign

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function openCard(evt, contentName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("about-card");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(contentName).style.display = "block";
+    evt.currentTarget.className += "active";
+}
