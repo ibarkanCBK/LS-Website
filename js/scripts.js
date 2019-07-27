b613337a6d313290c8096c6fc6f7b93e8305456f
ibarkanCBK <52602129+ibarkanCBK@users.noreply.github.com>
2019-07-27 17:56:26 -0400

    created resource structure

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,18 @@ function openCard(evt, contentName) {
     document.getElementById(contentName).style.display = "block";
     evt.currentTarget.className += "active";
 }
+
+
+function openSection(evt, contentName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("resource-card");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("r-tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(contentName).style.display = "block";
+    evt.currentTarget.className += "active";
+}
